7fac8c564e7020b05fafb8cb862c07e0bb2bae41
Timothy Kumbweza Banda <timobify@gmail.com>
2022-07-31 13:09:05 +0200

    new change

--- NEWSPAPER SUBSCRIPTION SYSTEM/Controllers/UsersController.cs
+++ NEWSPAPER SUBSCRIPTION SYSTEM/Controllers/UsersController.cs
@@ -49,8 +49,7 @@ namespace NEWSPAPER_SUBSCRIPTION_SYSTEM.Controllers
                     new Claim(ClaimTypes.Name, user.UserId.ToString())
                 }),
                 Expires = DateTime.UtcNow.AddDays(7),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
-                    SecurityAlgorithms.HmacSha256Signature)
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
             var token = tokenHandler.CreateToken(tokenDescriptor);
             var tokenString = tokenHandler.WriteToken(token);
